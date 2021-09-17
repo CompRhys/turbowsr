8540835829eb8a42abf5afacab7791d66dc08568
Rhys Goodall <reag2@cam.ac.uk>
2021-09-17 22:05:02 +0100

    fea: separate example file

--- examples/example.py
+++ examples/example.py
@@ -1,5 +1,5 @@
+# %%
 from pymatgen.core.structure import Structure
-import matplotlib
 import matplotlib.pyplot as plt
 
 from megnet.utils.models import load_model
@@ -7,25 +7,35 @@ from megnet.utils.models import load_model
 from turbowsr.opt import TuRBOWSR
 from turbo import Turbo1
 
-# s = Structure.from_file("/home/reag2/PhD/turbowsr/turbowsr/tests/POSCAR.mp-554710_AgAsC4S8(N2F3)2")
-s = Structure.from_file(
-    "/home/reag2/PhD/turbowsr/turbowsr/tests/POSCAR.tricky_symmetry"
-)
-# s = Structure.from_file("/home/reag2/PhD/turbowsr/turbowsr/tests/POSCAR.mp-862972_AcAgAu2")
+import numpy as np
+
+# %%
 
 model = load_model("Eform_MP_2019")
 
+# %%
+
+s = Structure.from_file("/home/reag2/PhD/turbowsr/turbowsr/tests/POSCAR.Fe3O4")
+# s = Structure.from_file("/home/reag2/PhD/turbowsr/turbowsr/tests/POSCAR.mp-554710_AgAsC4S8(N2F3)2")
+# s = Structure.from_file("/home/reag2/PhD/turbowsr/turbowsr/tests/POSCAR.tricky_symmetry")
+# s = Structure.from_file("/home/reag2/PhD/turbowsr/turbowsr/tests/POSCAR.mp-862972_AcAgAu2")
+# s = Structure.from_file("/home/reag2/PhD/turbowsr/turbowsr/tests/cubic_perovskite.cif")
+# s.scale_lattice(s.volume * 0.8)
+
+# %%
 f = TuRBOWSR(s, model)
 
 print(f.proto)
 print(f.chemsys)
+
+# %%
 
 turbo1 = Turbo1(
     f=f,  # Handle to objective function
     lb=f.lb,  # Numpy array specifying lower bounds
     ub=f.ub,  # Numpy array specifying upper bounds
     n_init=20,  # Number of initial bounds from an Latin hypercube design
-    max_evals=100,  # Maximum number of evaluations
+    max_evals=200,  # Maximum number of evaluations
     batch_size=10,  # How large batch size TuRBO uses
     verbose=True,  # Print information from each batch
     use_ard=True,  # Set to true if you want to use ARD for the GP kernel
@@ -43,15 +53,17 @@ fX = turbo1.fX  # Observed values
 ind_best = np.argmin(fX)
 f_best, x_best = fX[ind_best], X[ind_best, :]
 
-print("Best value found:\n\tf(x) = {f_best:.3f}")
+print(f"Best value found:\n\tf(x) = {f_best[0]:.3f}")
 print(dict(zip(f.params.keys(), x_best)))
-print(f.params)
 
-fig = plt.figure(figsize=(7, 5))
-matplotlib.rcParams.update({"font.size": 16})
-plt.plot(fX, "b.", ms=10)  # Plot all evaluated points as blue dots
-plt.plot(np.minimum.accumulate(fX), "r", lw=3)  # Plot cumulative minimum as a red line
+# %%
+
+fig = plt.figure(figsize=(5, 4))
+plt.plot(fX, ".", color="tab:blue", ms=10)  # Plot all evaluated points as blue dots
+plt.plot(np.minimum.accumulate(fX), color="tab:red", lw=3)  # Plot cumulative minimum as a red line
 plt.xlim([0, len(fX)])
 
 plt.tight_layout()
 plt.show()
+
+# %%
--- turbowsr/opt.py
+++ turbowsr/opt.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-from turbo import Turbo1
 
 from turbowsr.aflow import (
     get_proto_chemsys_params_from_struct,
@@ -50,60 +49,12 @@ class TuRBOWSR:
 
         self.params = dict(zip(self.params.keys(), x))
 
-        s = get_struct_from_proto_chemsys_params(self.proto, self.chemsys, self.params)
+        print(self.params)
 
         try:
+            s = get_struct_from_proto_chemsys_params(
+                self.proto, self.chemsys, self.params
+            )
             return self.model.predict_structure(s).item()
         except RuntimeError:
-            return 999
-
-
-if __name__ == "__main__":
-    from pymatgen.core.structure import Structure
-    import matplotlib
-    import matplotlib.pyplot as plt
-
-    from megnet.utils.models import load_model
-
-    # s = Structure.from_file("/home/reag2/PhD/turbowsr/turbowsr/tests/POSCAR.mp-554710_AgAsC4S8(N2F3)2")
-    s = Structure.from_file("/home/reag2/PhD/turbowsr/turbowsr/tests/POSCAR.mp-862972_AcAgAu2")
-
-    model = load_model("Eform_MP_2019")
-
-    f = TuRBOWSR(s, model)
-
-    turbo1 = Turbo1(
-        f=f,  # Handle to objective function
-        lb=f.lb,  # Numpy array specifying lower bounds
-        ub=f.ub,  # Numpy array specifying upper bounds
-        n_init=20,  # Number of initial bounds from an Latin hypercube design
-        max_evals=100,  # Maximum number of evaluations
-        batch_size=10,  # How large batch size TuRBO uses
-        verbose=True,  # Print information from each batch
-        use_ard=True,  # Set to true if you want to use ARD for the GP kernel
-        max_cholesky_size=2000,  # When we switch from Cholesky to Lanczos
-        n_training_steps=50,  # Number of steps of ADAM to learn the hypers
-        min_cuda=1024,  # Run on the CPU for small datasets
-        device="cpu",  # "cpu" or "cuda"
-        dtype="float64",  # float64 or float32
-    )
-
-    turbo1.optimize()
-
-    X = turbo1.X  # Evaluated points
-    fX = turbo1.fX  # Observed values
-    ind_best = np.argmin(fX)
-    f_best, x_best = fX[ind_best], X[ind_best, :]
-
-    print("Best value found:\n\tf(x) = {f_best:.3f}")
-    print(dict(zip(f.params.keys(), x_best)))
-    print(f.params)
-
-    fig = plt.figure(figsize=(7, 5))
-    matplotlib.rcParams.update({'font.size': 16})
-    plt.plot(fX, 'b.', ms=10)  # Plot all evaluated points as blue dots
-    plt.plot(np.minimum.accumulate(fX), 'r', lw=3)  # Plot cumulative minimum as a red line
-    plt.xlim([0, len(fX)])
-
-    plt.tight_layout()
-    plt.show()
+            return 66
